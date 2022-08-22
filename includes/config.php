eb599d45af377834abaf4ea4db1a8074c62f4abd
atomy <atom3123@gmail.com>
2022-08-22 17:58:44 +0700

    atomy update new

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,12 @@
 <?php
 //หากมีการเรียกไฟล์นี้โดยตรง
-if (preg_match('/config.php/i',$_SERVER['PHP_SELF'])) {
+if (preg_match('/config.php/i', $_SERVER['PHP_SELF'])) {
     Header('Location: ../index.php');
     die();
 }
 
 // Version
-define("_SCRIPT","SIM"); 
+define("_SCRIPT", "SIM");
 define('VERSION', '2.0');
 define('TIMESTAMP', time());
 
@@ -19,29 +19,29 @@ define('_footer_main_title', 'SIM 2.0');
 define('_text_footer1', 'SIM 2.0');
 define('_text_footer2', 'พัฒนาโดย นายชัดสกร พิกุลทอง ผู้อำนวยการโรงเรียนชื่นชมพิทยาคาร อำเภอชื่นชม จังหวัดมหาสารคาม');
 define('_text_footer3', 'โทร. 089946997 website : https://sim.ccpk.ac.th  email : atom3123@gmail.com');
-define('_google_email','atom3123@gmail.com');
-define('_google_email_password','tomtam3123');
-define('_facebook_wall','1');
-define('_facebook_comm','1');
-define('_facebook_api_key','257087804303083');
-define('_facebook_api_secret','eaa6344df3e9ab2048e39780ecee02c3');
+define('_google_email', 'atom3123@gmail.com');
+define('_google_email_password', 'tomtam3123');
+define('_facebook_wall', '1');
+define('_facebook_comm', '1');
+define('_facebook_api_key', '257087804303083');
+define('_facebook_api_secret', 'eaa6344df3e9ab2048e39780ecee02c3');
 
 
 //MySQL Connect
-define('DB_HOST','localhost');
-define('DB_NAME','stdkut');
-//define('DB_USERNAME','stdkut');
-//define('DB_PASSWORD','44012023');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('ISO','utf-8');
-define('WEB_TEMPLATES','shit');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'stdkut');
+define('DB_USERNAME', 'stdkut');
+define('DB_PASSWORD', '44012023');
+//define('DB_USERNAME','root');
+//define('DB_PASSWORD','');
+define('ISO', 'utf-8');
+define('WEB_TEMPLATES', 'shit');
 
-define('YEAR','2565');
-define('YEAR_LAST1','2564');
-define('YEAR_LAST2','2563');
-define('YEAR_LAST3','2562');
-define('YEAR_LAST4','2561');
+define('YEAR', '2565');
+define('YEAR_LAST1', '2564');
+define('YEAR_LAST2', '2563');
+define('YEAR_LAST3', '2562');
+define('YEAR_LAST4', '2561');
 
 
 define('WEB_PATH', '/home/stdkut/public_html');
@@ -72,8 +72,8 @@ define('WEB_PATH_IMG_NEWS', '/home/stdkut/public_html/img/news/');
 define('WEB_URL_IMG_NEWS_RAN', 'https://sim.ccpk.ac.th/img/news/');
 define('WEB_PATH_IMG_NEWS_RAN', '/home/stdkut/public_html/img/news/');
 
-define('WEB_URL_IMG_SCHOOL','https://sim.ccpk.ac.th/img/school/');
-define('WEB_PATH_IMG_SCHOOL','/home/stdkut/public_html/img/school/');
+define('WEB_URL_IMG_SCHOOL', 'https://sim.ccpk.ac.th/img/school/');
+define('WEB_PATH_IMG_SCHOOL', '/home/stdkut/public_html/img/school/');
 
 //define('WEB_PATH', '/usr/local/www/apache22/data/maxtom');
 //define('WEB_URL', 'https://thaiesan.sytes.net/maxtom');
@@ -96,169 +96,169 @@ define('_I_IPER_W', '150'); // ขนาดความกว้างของ�
 define('_I_IPER_H', '200'); // ขนาดความสูงของรูปภาพสามชิกและผู้ดูแลระบบ
 define('_I_ISTU_W', '150'); // ขนาดความกว้างของรูปภาพสามชิกและผู้ดูแลระบบ
 define('_I_ISTU_H', '200'); // ขนาดความสูงของรูปภาพสามชิกและผู้ดูแลระบบ
-define('_I_MOTOR_W', '200'); 
-define('_I_MOTOR_H', '200'); 
-define('_I_NEWS_W', '300'); 
-define('_I_NEWS_H', '300'); 
-define('_I_NEWS_RAN_W', '2048'); 
-define('_I_NEWS_RAN_H', '800'); 
+define('_I_MOTOR_W', '200');
+define('_I_MOTOR_H', '200');
+define('_I_NEWS_W', '300');
+define('_I_NEWS_H', '300');
+define('_I_NEWS_RAN_W', '2048');
+define('_I_NEWS_RAN_H', '800');
 
-define('_I_LOGO_SIG_W', '150'); 
-define('_I_LOGO_SIG_H', '150'); 
+define('_I_LOGO_SIG_W', '150');
+define('_I_LOGO_SIG_H', '150');
 
 
 //MySQL table
-define('TB_BIRTH','web_birth');
+define('TB_BIRTH', 'web_birth');
 
-define('TB_ACTIVEUSER','web_activeuser');
-define('TB_ADMIN','web_admin');
-define('TB_ADMIN_MENU','web_admin_menu');
-define('TB_ADMIN_RESET','web_admin_reset');
-define('TB_ADMIN_ONLINE','web_admin_online');
-define('TB_ADMIN_GROUP','web_admin_group');
-define('TB_AMPHUR','web_amphur');
-define('TB_BAD','web_bad');
-define('TB_BADDATA','web_baddata');
-define('TB_BADLEVEL','web_badlevel');
-define('TB_BADTAIL','web_badtail');
-define('TB_BAD_BAK','web_bad_bak');
-define('TB_BEST','web_best');
-define('TB_BESTTAIL','web_besttail');
-define('TB_BEST_BAK','web_best_bak');
-define('TB_BHOME','web_bhome');
-define('TB_BHOMETAIL','web_bhometail');
-define('TB_BLOCK','web_block');
-define('TB_CARD_STUDENT','web_card_student');
-define('TB_CHCLASS','web_chclass');
-define('TB_CHK_CHCLASS','web_chk_chclass');
-define('TB_CHK_CHMCLASS','web_chk_mclass');
-define('TB_CLASS','web_class');
-define('TB_CLASS_GROUP','web_class_group');
-define('TB_CLASS_PERSON','web_class_person');
-define('TB_DAM','web_dam');
-define('TB_DAMTAIL','web_damtail');
-define('TB_DOM','web_dom');
-define('TB_EXIT','web_exit');
-define('TB_EXITTAIL','web_exittail');
-define('TB_LA','web_la');
-define('TB_LATAIL','web_latail');
-define('TB_GCOLOR','web_gcolor');
-define('TB_GOHOME','web_gohome');
-define('TB_GOHOMETAIL','web_gohometail');
-define('TB_GOOD','web_good');
-define('TB_GOODTAIL','web_goodtail');
-define('TB_GOODDATA','web_gooddata');
-define('TB_GOODLEVEL','web_goodlevel');
-define('TB_GOOD_BAK','web_good_bak');
-define('TB_GOTOSCH','web_gotosch');
-define('TB_GRADE_STUDENT','web_grade_student');
-define('TB_MENU','web_menu');
-define('TB_MESSAGE','web_message');
-define('TB_MESSAGE_CHECK','web_message_check');
-define('TB_MESSAGE_COM','web_message_comment');
-define('TB_MODULES','web_modules');
-define('TB_NEWS','web_news');
-define('TB_NEWS_CATE','web_news_category');
-define('TB_NEWS_COM','web_news_comment');
-define('TB_ORG','web_org');
-define('TB_PERSON','web_person');
-define('TB_PERSON_GROUP','web_person_group');
-define('TB_PERSON_MENU','web_person_menu');
-define('TB_PERSON_RESET','web_person_reset');
-define('TB_PERSON_ONLINE','web_person_online');
-define('TB_PROVINCE','web_province');
-define('TB_PUT','web_put');
-define('TB_PUTPER','web_putper');
-define('TB_PUTLEVEL','web_putlevel');
-define('TB_PUTTAIL','web_puttail');
-define('TB_PUTTAIL_BAK','web_puttail_bak');
-define('TB_RUBRONG','web_rubrong');
-define('TB_RUBRONGTAIL','web_rubrongtail');
-define('TB_SESSION','web_session');
+define('TB_ACTIVEUSER', 'web_activeuser');
+define('TB_ADMIN', 'web_admin');
+define('TB_ADMIN_MENU', 'web_admin_menu');
+define('TB_ADMIN_RESET', 'web_admin_reset');
+define('TB_ADMIN_ONLINE', 'web_admin_online');
+define('TB_ADMIN_GROUP', 'web_admin_group');
+define('TB_AMPHUR', 'web_amphur');
+define('TB_BAD', 'web_bad');
+define('TB_BADDATA', 'web_baddata');
+define('TB_BADLEVEL', 'web_badlevel');
+define('TB_BADTAIL', 'web_badtail');
+define('TB_BAD_BAK', 'web_bad_bak');
+define('TB_BEST', 'web_best');
+define('TB_BESTTAIL', 'web_besttail');
+define('TB_BEST_BAK', 'web_best_bak');
+define('TB_BHOME', 'web_bhome');
+define('TB_BHOMETAIL', 'web_bhometail');
+define('TB_BLOCK', 'web_block');
+define('TB_CARD_STUDENT', 'web_card_student');
+define('TB_CHCLASS', 'web_chclass');
+define('TB_CHK_CHCLASS', 'web_chk_chclass');
+define('TB_CHK_CHMCLASS', 'web_chk_mclass');
+define('TB_CLASS', 'web_class');
+define('TB_CLASS_GROUP', 'web_class_group');
+define('TB_CLASS_PERSON', 'web_class_person');
+define('TB_DAM', 'web_dam');
+define('TB_DAMTAIL', 'web_damtail');
+define('TB_DOM', 'web_dom');
+define('TB_EXIT', 'web_exit');
+define('TB_EXITTAIL', 'web_exittail');
+define('TB_LA', 'web_la');
+define('TB_LATAIL', 'web_latail');
+define('TB_GCOLOR', 'web_gcolor');
+define('TB_GOHOME', 'web_gohome');
+define('TB_GOHOMETAIL', 'web_gohometail');
+define('TB_GOOD', 'web_good');
+define('TB_GOODTAIL', 'web_goodtail');
+define('TB_GOODDATA', 'web_gooddata');
+define('TB_GOODLEVEL', 'web_goodlevel');
+define('TB_GOOD_BAK', 'web_good_bak');
+define('TB_GOTOSCH', 'web_gotosch');
+define('TB_GRADE_STUDENT', 'web_grade_student');
+define('TB_MENU', 'web_menu');
+define('TB_MESSAGE', 'web_message');
+define('TB_MESSAGE_CHECK', 'web_message_check');
+define('TB_MESSAGE_COM', 'web_message_comment');
+define('TB_MODULES', 'web_modules');
+define('TB_NEWS', 'web_news');
+define('TB_NEWS_CATE', 'web_news_category');
+define('TB_NEWS_COM', 'web_news_comment');
+define('TB_ORG', 'web_org');
+define('TB_PERSON', 'web_person');
+define('TB_PERSON_GROUP', 'web_person_group');
+define('TB_PERSON_MENU', 'web_person_menu');
+define('TB_PERSON_RESET', 'web_person_reset');
+define('TB_PERSON_ONLINE', 'web_person_online');
+define('TB_PROVINCE', 'web_province');
+define('TB_PUT', 'web_put');
+define('TB_PUTPER', 'web_putper');
+define('TB_PUTLEVEL', 'web_putlevel');
+define('TB_PUTTAIL', 'web_puttail');
+define('TB_PUTTAIL_BAK', 'web_puttail_bak');
+define('TB_RUBRONG', 'web_rubrong');
+define('TB_RUBRONGTAIL', 'web_rubrongtail');
+define('TB_SESSION', 'web_session');
 
-define('TB_SPACIAL','web_spacial');
-define('TB_SPACIALTAIL','web_spacialtail');
-define('TB_SPACIALTAIL_BAK','web_spacialtail_bak');
-define('TB_MOTOR','web_motor');
-define('TB_MOTORTAIL','web_motortail');
-define('TB_MOTORTAIL_BAK','web_motortail_bak');
-define('TB_ENT','web_ent');
-define('TB_ENTTAIL','web_enttail');
+define('TB_SPACIAL', 'web_spacial');
+define('TB_SPACIALTAIL', 'web_spacialtail');
+define('TB_SPACIALTAIL_BAK', 'web_spacialtail_bak');
+define('TB_MOTOR', 'web_motor');
+define('TB_MOTORTAIL', 'web_motortail');
+define('TB_MOTORTAIL_BAK', 'web_motortail_bak');
+define('TB_ENT', 'web_ent');
+define('TB_ENTTAIL', 'web_enttail');
 
-define('TB_STUDENT','web_student');
-define('TB_STUDENT_BAK','web_student_bak');
-define('TB_STUDENT_MENU','web_student_menu');
-define('TB_STUDENT_RESET','web_student_reset');
-define('TB_STUDENT_ONLINE','web_student_online');
+define('TB_STUDENT', 'web_student');
+define('TB_STUDENT_BAK', 'web_student_bak');
+define('TB_STUDENT_MENU', 'web_student_menu');
+define('TB_STUDENT_RESET', 'web_student_reset');
+define('TB_STUDENT_ONLINE', 'web_student_online');
 
-define('TB_TUMBON','web_tumbon');
-define('TB_USER','web_user');
-define('TB_USER_MENU','web_user_menu');
-define('TB_USER_RESET','web_user_reset');
-define('TB_USER_LEVEL','web_user_level');
-define('TB_USER_ONLINE','web_user_online');
-define('TB_USER_GROUP','web_user_group');
-define('TB_VOTE','web_vote');
-define('TB_VISITOR','web_visitors');
+define('TB_TUMBON', 'web_tumbon');
+define('TB_USER', 'web_user');
+define('TB_USER_MENU', 'web_user_menu');
+define('TB_USER_RESET', 'web_user_reset');
+define('TB_USER_LEVEL', 'web_user_level');
+define('TB_USER_ONLINE', 'web_user_online');
+define('TB_USER_GROUP', 'web_user_group');
+define('TB_VOTE', 'web_vote');
+define('TB_VISITOR', 'web_visitors');
 
-define('TB_COUNCIL','web_council');
-define('TB_COUNTAIL','web_counciltail');
-define('TB_WHITECLASS','web_whiteclass');
-define('TB_WHITECLTAIL','web_whitecltail');
-define('TB_AFFAIRS','web_affairs');
-define('TB_AFFTAIL','web_afftail');
+define('TB_COUNCIL', 'web_council');
+define('TB_COUNTAIL', 'web_counciltail');
+define('TB_WHITECLASS', 'web_whiteclass');
+define('TB_WHITECLTAIL', 'web_whitecltail');
+define('TB_AFFAIRS', 'web_affairs');
+define('TB_AFFTAIL', 'web_afftail');
 
-define('TB_NUT','web_nut');
-define('TB_TUNBON','web_tunbon');
+define('TB_NUT', 'web_nut');
+define('TB_TUNBON', 'web_tunbon');
 
-define('_I_SCHOOL_W', '1024'); 
+define('_I_SCHOOL_W', '1024');
 define('_I_SCHOOL_H', '373');
 
-define('_I_AREA_W', '1024'); 
-define('_I_AREA_H', '768'); 
+define('_I_AREA_W', '1024');
+define('_I_AREA_H', '768');
 
-define("TB_AREA","web_area");
-define('TB_AREA_USER','web_area_user');
-define('TB_AREA_USER_GROUP','web_area_user_group');
-define('TB_AREA_USER_MENU','web_area_user_menu');
-define('TB_AREA_USER_RESET','web_area_user_reset');
-define('TB_AREA_USER_ONLINE','web_area_user_online');
-define('TB_AREA_USER_PERM','web_area_user_perm');
-define("TB_AREA_BOSS","web_area_boss");
-define("TB_AREA_BOSS_GR","web_area_boss_gr");
-define('TB_AREA_LINE','web_area_line');
+define("TB_AREA", "web_area");
+define('TB_AREA_USER', 'web_area_user');
+define('TB_AREA_USER_GROUP', 'web_area_user_group');
+define('TB_AREA_USER_MENU', 'web_area_user_menu');
+define('TB_AREA_USER_RESET', 'web_area_user_reset');
+define('TB_AREA_USER_ONLINE', 'web_area_user_online');
+define('TB_AREA_USER_PERM', 'web_area_user_perm');
+define("TB_AREA_BOSS", "web_area_boss");
+define("TB_AREA_BOSS_GR", "web_area_boss_gr");
+define('TB_AREA_LINE', 'web_area_line');
 
-define('TB_SCHOOL','web_school');
-define('TB_SCHOOL_CODE','web_school_code');
-define('TB_SCHOOL_GR','web_school_gr');
-define('TB_SCHOOL_LEVEL','web_school_level');
-define('TB_SCHOOL_FINAL','web_school_final');
-define('TB_SCHOOL_STU','web_school_stu');
-define('TB_SCHOOL_STU_YEAR','web_school_stu_year');
-define('TB_SCHOOL_STU_TERM','web_school_stu_term');
-define('TB_SCHOOL_CONFIG','web_school_config');
+define('TB_SCHOOL', 'web_school');
+define('TB_SCHOOL_CODE', 'web_school_code');
+define('TB_SCHOOL_GR', 'web_school_gr');
+define('TB_SCHOOL_LEVEL', 'web_school_level');
+define('TB_SCHOOL_FINAL', 'web_school_final');
+define('TB_SCHOOL_STU', 'web_school_stu');
+define('TB_SCHOOL_STU_YEAR', 'web_school_stu_year');
+define('TB_SCHOOL_STU_TERM', 'web_school_stu_term');
+define('TB_SCHOOL_CONFIG', 'web_school_config');
 
 //grade online
-define("TB_GD_TRAN","web_grade_transcript");
-define("TB_GD_TEACH","web_grade_teacher");
-define("TB_GD_SUBJ","web_grade_subject");
-define("TB_GD_GROUP","web_grade_group");
-define("TB_GD_ACC","web_grade_activity");
-define("TB_GD_ATT","web_grade_attribute");
-define("TB_GD_CAP","web_grade_capacity");
-define("TB_GD_TRANAC","web_grade_tranAC");
-define("TB_GD_TRANATT","web_grade_tranATT");
-define("TB_GD_TRANCAP","web_grade_tranCAP");
-define("TB_GD_TERM","web_grade_term");
+define("TB_GD_TRAN", "web_grade_transcript");
+define("TB_GD_TEACH", "web_grade_teacher");
+define("TB_GD_SUBJ", "web_grade_subject");
+define("TB_GD_GROUP", "web_grade_group");
+define("TB_GD_ACC", "web_grade_activity");
+define("TB_GD_ATT", "web_grade_attribute");
+define("TB_GD_CAP", "web_grade_capacity");
+define("TB_GD_TRANAC", "web_grade_tranAC");
+define("TB_GD_TRANATT", "web_grade_tranATT");
+define("TB_GD_TRANCAP", "web_grade_tranCAP");
+define("TB_GD_TERM", "web_grade_term");
 
-define('TB_YEAR','web_year');
+define('TB_YEAR', 'web_year');
 
-define("TB_REGISTER","web_register");
+define("TB_REGISTER", "web_register");
 
-define('_Facebook_Api_Key','334116950629804');
-define('_Facebook_Api_Secret','efab57865b2d52cfb4f10c48b41d6e4a');
+define('_Facebook_Api_Key', '334116950629804');
+define('_Facebook_Api_Secret', 'efab57865b2d52cfb4f10c48b41d6e4a');
 
-define('_GOOGLE_Api_ID','770360926176-jg73mvl07k38ue2rk19lkh2jbpt7jqu5.apps.googleusercontent.com');
-define('_GOOGLE_Api_Secret','1HyGV7WGKOlOZN8ODnzMolW3');
+define('_GOOGLE_Api_ID', '770360926176-jg73mvl07k38ue2rk19lkh2jbpt7jqu5.apps.googleusercontent.com');
+define('_GOOGLE_Api_Secret', '1HyGV7WGKOlOZN8ODnzMolW3');
 
-define('LINE_API',"https://notify-api.line.me/api/notify");
+define('LINE_API', "https://notify-api.line.me/api/notify");
